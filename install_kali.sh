95fff00b1ae10c4611c3964e14b3586fb2cebfe3
OlivierProTips <olivierprotips@gmail.com>
2023-12-30 22:47:17 +0100

    Adding some tools

--- install_kali.sh
+++ install_kali.sh
@@ -151,6 +151,18 @@ sudo chmod +x /usr/local/bin/vpnchoice
 echo "alias vpnchoice='sudo vpnchoice'" >> ~/.zshrc
 sudo python3 -m pip install simple-term-menu
 
+# TOOLS
+echo "=============================="
+echo "Installing TOOLS"
+echo "=============================="
+echo '#!/bin/bash' > ~/Tools/update_tools.sh
+echo 'wget https://raw.githubusercontent.com/pentestmonkey/php-reverse-shell/master/php-reverse-shell.php -O ~/Tools/rev.php' >> ~/Tools/update_tools.sh
+echo 'wget https://github.com/carlospolop/PEASS-ng/releases/latest/download/linpeas.sh -O ~/Tools/linpeas.sh' >> ~/Tools/update_tools.sh
+echo 'wget https://github.com/DominicBreuker/pspy/releases/latest/download/pspy64 -O ~/Tools/pspy64' >> ~/Tools/update_tools.sh
+echo 'wget https://github.com/DominicBreuker/pspy/releases/latest/download/pspy32 -O ~/Tools/pspy32' >> ~/Tools/update_tools.sh
+chmod +x ~/Tools/update_tools.sh
+~/Tools/update_tools.sh
+
 # MANUAL STEPS
 echo " ------------------------------------- "
 echo "| MANUAL STEPS                        |"
