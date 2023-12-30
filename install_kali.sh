8557f9eb0b8379cc1538ea4e52555300babb25f7
OlivierProTips <olivierprotips@gmail.com>
2023-12-30 22:47:16 +0100

    Add apt update

--- install_kali.sh
+++ install_kali.sh
@@ -16,9 +16,10 @@ sudo apt update
 echo "=============================="
 echo "Installing VSCODE"
 echo "=============================="
-sudo apt install software-properties-common apt-transport-https wget
+sudo apt install software-properties-common apt-transport-https wget -y
 wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
 sudo add-apt-repository "deb [arch=${1,,}] https://packages.microsoft.com/repos/vscode stable main"
+sudo apt update
 sudo apt install code -y
 
 code --install-extension donjayamanne.python-extension-pack --force
