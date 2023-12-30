62e0a69d1eafc09a0edca195086391929268ffe7
OlivierProTips <olivierprotips@gmail.com>
2023-12-30 22:47:16 +0100

    adding echo

--- install_kali.sh
+++ install_kali.sh
@@ -10,7 +10,12 @@ cd /tmp
 mkdir -p kali_install
 cd kali_install
 
+sudo apt update
+
 # VSCODE
+echo "=============================="
+echo "Installing VSCODE"
+echo "=============================="
 sudo apt install software-properties-common apt-transport-https wget
 wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
 sudo add-apt-repository "deb [arch=${1,,}] https://packages.microsoft.com/repos/vscode stable main"
@@ -21,29 +26,47 @@ code --install-extension donjayamanne.git-extension-pack --force
 code --install-extension ms-vscode.sublime-keybindings --force
 
 # TERMINATOR
+echo "=============================="
+echo "Installing TERMINATOR"
+echo "=============================="
 sudo apt install terminator -y
 wget https://raw.githubusercontent.com/OlivierProTips/HackNotes/main/Terminator/config
 mkdir -p ~/.config/terminator
 mv config ~/.config/terminator/config
 
 # VIM
+echo "=============================="
+echo "Setting VIM"
+echo "=============================="
 mv .vimrc ~
 
 # ZSH
+echo "=============================="
+echo "Setting ZSH"
+echo "=============================="
 mv .zshrc ~
 
 # UPLOAD
+echo "=============================="
+echo "Installing UPLOAD"
+echo "=============================="
 wget https://raw.githubusercontent.com/OlivierProTips/HackNotes/main/scripts/upload_file_wget.sh
 sudo mv upload_file_wget.sh /usr/local/bin
 sudo chmod +x /usr/local/bin/upload_file_wget.sh
 
 # MONIP
+echo "=============================="
+echo "Installing MONIP"
+echo "=============================="
 wget https://raw.githubusercontent.com/OlivierProTips/HackNotes/main/scripts/monip.sh
 sudo mv monip.sh /usr/local/bin
 sudo chmod +x /usr/local/bin/monip.sh
 sudo ln -s /usr/local/bin/monip.sh /usr/local/bin/monip
 
 # NMAPER
+echo "=============================="
+echo "Installing NMAPER"
+echo "=============================="
 wget https://raw.githubusercontent.com/OlivierProTips/nmaper/master/nmaper.py
 sudo mv nmaper.py /usr/local/bin
 sudo chmod +x /usr/local/bin/nmaper.py
